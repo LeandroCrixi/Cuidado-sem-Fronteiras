47839df38d97ad1006cf95d4f4cf9e78dd583fdd
Leo Crixi <leandro.crixi@hotmail.com>
2026-06-19 13:25:29 +0200

    Header

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+import { currentYear, myBackground } from "./modules/dom.js";
+import { GlobalHeader } from "./components/header.js";
+import { initBurgerMenu } from "./components/burgerMenu.js";
+import { initCal } from "./components/cal.js";
+import { GlobalFooter } from "./components/footer.js";
+
+// currentYear()
+initBurgerMenu();
+myBackground();
+initCal("catch-up");
+initCal("private-coaching-clients");
+initCal("exec-coaching-clients");
